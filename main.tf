45cd21440bc9ef352d1a394b367f46c147820e1f
ashrithap03 <158589076+ashrithap03@users.noreply.github.com>
2024-07-15 11:24:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_configuration" "web_server_as" {
-    image_id           = "ami-0eaf7c3456e7b5b68"
+    image_id           = "ami-01fccab91b456acc2"
     instance_type = "t2.micro"
-    key_name = "docker-pipeline"
+    key_name = "ashritha54-opc"
 }
    
 
@@ -9,7 +9,7 @@ resource "aws_launch_configuration" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-01801d057d263ad13", "subnet-0bcc1a1ec83ddb757"]
+     subnets = ["subnet-08285081203e130b9", "subnet-0ea941f7a8af46ef1"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
